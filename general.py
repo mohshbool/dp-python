7ec4cb74e262b6b823561d5a98278ac2321859f4
Mohammad Shbool <mohammad.shboul@reachworld.com>
2024-11-17 18:43:54 +0300

    grid_paths

--- general.py
+++ general.py
@@ -13,3 +13,19 @@ def coins(n, d):
     return k
 
 print(coins(140, [50, 25, 10, 5, 1]))
+
+def grid_paths(m, n):
+    dp = [[0] * n for _ in range(m)]
+    
+    for i in range(m):
+        dp[i][0] = 1
+    for j in range(n):
+        dp[0][j] = 1
+    
+    for i in range(1, m):
+        for j in range(1, n):
+            dp[i][j] = dp[i-1][j] + dp[i][j-1]
+    
+    return dp[m-1][n-1]
+
+print(grid_paths(3, 4))
